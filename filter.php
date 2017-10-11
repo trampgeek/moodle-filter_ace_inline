9cc2ac22aa6b69f594a23b6aeb7534314821c928
root <root@richard.richardnz.com>
2017-10-11 14:04:35 +1300

    simple question filter version 1

---
+++ filter.php
@@ -0,0 +1,99 @@
+<?php
+
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Basic email protection filter.
+ *
+ * @package    filter
+ * @subpackage simplequestion
+ * @copyright  2017 Richard Jones (https://richardnz.net)
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ * 
+ */
+
+defined('MOODLE_INTERNAL') || die();
+/**
+ * This class looks for question tags in Moodle text and
+ * replaces them with questions from the question bank.
+ * Tags have the format {QUESTION:link text|xxx} where
+ *                               link text is the text that links to the question 
+ *                               xxx is the id of a question
+ * from the bank in the current course.
+ */
+class filter_simplequestion extends moodle_text_filter {
+
+  function filter($text, array $options = array()) {
+  global $PAGE;
+  // Might need to change these at some point - eg to double curlies
+  $START_TAG = '{QUESTION:';
+  $END_TAG = '}';
+
+  $renderer = $PAGE->get_renderer('filter_simplequestion');
+
+    // Basic tests to avoid work
+    if (!is_string($text)) {
+      // non string data can not be filtered anyway
+      return $text;
+    }
+    if (strpos($text, '{') === false) {
+      // Do a quick check to see if we have curlies
+      return $text;
+    }
+
+    // There may be a question or questions in here somewhere so continue ...
+    // Get the question numbers and positions in the text and call the
+    // renderer to deal with them
+    $text = filter_simplequestion_insert_questions($text, $START_TAG, $END_TAG, $renderer);   
+
+    return $text;
+  }
+
+}
+/**
+*
+* function to replace question filter text with actual question
+*
+* params:  string containing patterns, pattern start, pattern end, renderer
+*/
+function filter_simplequestion_insert_questions($str, $needle, $limit, $renderer) {
+  
+  $newstring = $str;
+  While (strpos($newstring, $needle) !== false) {
+    $initpos = strpos($newstring, $needle);
+    if ($initpos !== false) {
+       $pos = $initpos + strlen($needle);  //get up to string
+       $endpos = strpos($newstring, $limit);
+       $data = substr($newstring, $pos, $endpos - $pos); // extract question data
+       // Todo: add some sanity checks here, trim the string etc
+       
+       // get the link text
+       $endlinkpos = strpos($data, '|');
+       $linktext = substr($data, 0, $endlinkpos);
+       
+       // get the question number
+       $number = substr($data, $endlinkpos + 1, $endpos - $endlinkpos);
+       
+       // Render the question link
+       $question = $renderer->get_question($number, $linktext); 
+       
+       // Update the text to replace the filtered string
+       $newstring = substr_replace($newstring, $question, $initpos, $endpos - $initpos + 1);
+       $initpos = $endpos + 1;
+    }
+  }
+  return $newstring;
+}
---
+++ renderer.php
@@ -0,0 +1,55 @@
+<?php
+
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Custom renderer class for filter_simplequestion
+ *
+ * @package    filter
+ * @subpackage simplequestion
+ * @copyright  2017 Richard Jones (https://richardnz.net/)
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+class filter_simplequestion_renderer extends plugin_renderer_base {
+
+  /**
+  *
+  * Given a question id, show the preview.php page
+  *
+  */
+  public function get_question($number, $linktext) {
+    global $CFG;
+    // What's the most sensible thing to do here?
+    // I'm thinking return a popup link to preview.php
+    // Todo: look at this: https://moodle.org/mod/forum/discuss.php?d=332254
+
+    $url = '/filter/simplequestion/preview.php'; 
+
+    // Now the question number will be visible within the link, do we care?
+    // Not for the simplequestion version anyway
+    $link = new moodle_url($url, array('id'=>$number));
+    
+    // Check for link text
+    if ($linktext === '') { $linktext = get_string('link_text', 'filter_question'); }
+    
+    // Todo: Add an option to have the link inline or as a popup
+    $text = $this->output->action_link($link, $linktext, new popup_action('click', $link)); 
+    return $text;
+  }
+}
