e16cd75c670df755b6b14151ced0c75b715add70
root <root@richard.richardnz.com>
2017-10-11 15:36:20 +1300

    updated Moodle version

--- version.php
+++ version.php
@@ -27,5 +27,5 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->version   = 2017101000;        // The current plugin version (Date: YYYYMMDDXX)
-$plugin->requires  = 2017100600;        // Requires this Moodle version
+$plugin->requires  = 2017051500;        // Requires this Moodle version
 $plugin->component = 'filter_simplequestion'; // Full name of the plugin (used for diagnostics)
