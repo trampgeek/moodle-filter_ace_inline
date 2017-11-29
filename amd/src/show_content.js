1595f099f3db2daffa2e76b67c20f178d222817e
Richard Jones <richard@ib-computing.com>
2017-11-29 14:20:00 +1300

    deleted pix folder

--- amd/src/show_content.js
+++ amd/src/show_content.js
@@ -2,7 +2,6 @@ define(['jquery'], function($) {
  
     return {
         init: function(content) {
- 
             // Put whatever you like here. $ is available
             // to you as normal.
             $(".filter_simplemodal_content").click(function() {
